813535cd955c5bbaa8eaf6e50aa747d61f81e781
Islomov Diyor <diyor.islomov@gmail.com>
2015-03-18 20:31:15 +0000

    Audio related bug fixes

--- ECAudioManager.h
+++ ECAudioManager.h
@@ -1,3 +1,5 @@
+//  Copyright (c) 2015 Diyor Islomov. All rights reserved.
+//	i.dior@mail.ru || diyor.islomov@gmail.com || @iDiyor 
 #ifndef __EC_AUDIO_MANAGER_H__ 
 #define __EC_AUDIO_MANAGER_H__
 
