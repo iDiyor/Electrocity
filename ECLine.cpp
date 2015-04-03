89c9e942d649b4ade9da4375d0cdefe720fbe6dd
Islomov Diyor <diyor.islomov@gmail.com>
2015-04-03 22:02:33 +0100

    Now line is responsible for collision detection with a building. Function moved from building to line. Better collision detection logic implemented

--- ECLine.cpp
+++ ECLine.cpp
@@ -2,6 +2,7 @@
 //	diyor.islomov@gmail.com || @iDiyor 
 #include "ECLine.h"
 #include "ECTower.h"
+#include "ECBuilding.h"
 
 ECLine::ECLine()
 {
@@ -44,8 +45,37 @@ void ECLine::ResetLineBetweenTowers(ECTower* tower_a, ECTower* tower_b)
 	this->setRotation(angle_in_deg);
 	this->setScaleX(length / this->getContentSize().width);
 
+	line_start_point_ = tower_a_point;
+	line_end_point_   = tower_b_point;
+
 	//_beginLinePoint = this->getPosition();
-	line_end_point_ = tower_b_point;//ccpAdd(this->getPosition(), ccpMult(ccp(cos(angle_in_rad), sin(angle_in_rad)), length));
+	//line_end_point_ = tower_b_point;//ccpAdd(this->getPosition(), ccpMult(ccp(cos(angle_in_rad), sin(angle_in_rad)), length));
+}
+bool ECLine::CheckCollisionWithBuilding(ECBuilding* building) {
+
+	CCPoint building_position = building->getPosition();
+	CCSize building_sprite_size = building->getContentSize();
+	float building_sprite_scale = building->getScale();
+
+	CCPoint left_bottom_point	= ccp(building_position.x - (building_sprite_size.width * (0.09375f * building_sprite_scale)),
+									  building_position.y + (building_sprite_size.height * (0.269231f * building_sprite_scale)));
+	CCPoint left_top_point		= ccp(left_bottom_point.x, left_bottom_point.y + (building_sprite_size.height * (0.15385f * building_sprite_scale)));
+	CCPoint right_top_point		= ccp(left_top_point.x + (building_sprite_size.width * (0.125f * building_sprite_scale)), left_top_point.y);
+	CCPoint right_bottom_point	= ccp(right_top_point.x, left_bottom_point.y);
+
+	float s = 0 , t = 0;
+
+	if ((ccpLineIntersect(line_start_point_, line_end_point_, left_bottom_point, left_top_point, &s, &t) && s >= 0 && s <= 1 && t >= 0 && t <= 1) ||
+		(ccpLineIntersect(line_start_point_, line_end_point_, left_top_point, right_top_point, &s, &t) && s >= 0 && s <= 1 && t >= 0 && t <= 1) ||
+		(ccpLineIntersect(line_start_point_, line_end_point_, right_top_point, right_bottom_point, &s, &t) && s >= 0 && s <= 1 && t >= 0 && t <= 1) || 
+		(ccpLineIntersect(line_start_point_, line_end_point_, left_bottom_point, right_bottom_point, &s, &t) && s >= 0 && s <= 1 && t >= 0 && t <= 1)) 
+	{
+		//building->SetContactState(YES_CONTACT);
+		return true;
+	}
+
+	//building->SetContactState(NO_CONTACT);
+	return false;
 }
 CCPoint ECLine::GetLineEndPoint()
 {
--- ECLine.h
+++ ECLine.h
@@ -8,6 +8,7 @@
 using namespace cocos2d;
 
 class ECTower;
+class ECBuilding;
 
 class ECLine : public CCSprite
 {
@@ -19,11 +20,12 @@ public:
 	~ECLine();
 	
 	void ResetLineBetweenTowers(ECTower* tower_a, ECTower* tower_b);
+	bool CheckCollisionWithBuilding(ECBuilding* building);
 
 	CCPoint GetLineEndPoint();
 private:
 
-	//CCPoint _beginLinePoint;
+	CCPoint line_start_point_;
 	CCPoint line_end_point_;
 };
 #endif //__EC_LINE_H__
